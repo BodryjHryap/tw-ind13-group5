d03fe057989e6f52fbc69cf8e5c59ace0736cd71
bodryjhryap <senya.akimov@mail.ru>
2024-02-12 23:26:41 +0500

    init project

---
+++ src/main/java/com/skypro/twind13group5/TwInd13Group5Application.java
@@ -0,0 +1,13 @@
+package com.skypro.twind13group5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwInd13Group5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwInd13Group5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/twind13group5/TwInd13Group5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.twind13group5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwInd13Group5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
