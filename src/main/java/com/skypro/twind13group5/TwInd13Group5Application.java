8ad55a1ae5fc8a48713b574ea3b0ffe1d6feb1cf
Денис <DenisKhr@yandex.ru>
2024-03-03 00:07:11 +1000

    Сделан этап 0

--- src/main/java/com/skypro/twind13group5/TwInd13Group5Application.java
+++ src/main/java/com/skypro/twind13group5/TwInd13Group5Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TwInd13Group5Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/skypro/twind13group5/service/UserService.java
@@ -0,0 +1,38 @@
+package com.skypro.twind13group5.service;
+
+import com.skypro.twind13group5.model.User;
+import com.skypro.twind13group5.repository.UserRepository;
+import com.skypro.twind13group5.enums.UserType;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(long telegramId, String nickName, UserType userType) {
+
+        User newUser = new User(telegramId, nickName, userType);
+        User user = userRepository.findByTelegramId(telegramId);
+        if (user == null) {
+            userRepository.save(newUser);
+            return newUser;
+        }
+        return user;
+    }
+
+    @Transactional
+    public User findUserByTelegramId(long telegramId) {
+        return userRepository.findByTelegramId(telegramId);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+}
